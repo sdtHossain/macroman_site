fa4c1990b8f5667a7779cf48e3092f21297a910b
Oritro Ahmed <oritromax@gmail.com>
2020-05-05 14:19:12 +0600

    Site Design Updated

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -8,12 +8,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class BaseController extends AbstractController
 {
     /**
-     * @Route("/", name="home_controller")
+     * @Route("/", name="base_home")
      */
     public function index()
     {
         return $this->render('base/home.html.twig', [
             'controller_name' => 'BaseController',
         ]);
+    }
+
+    /**
+     * @Route("/apply", name="home_apply")
+     */
+    public function jobApply()
+    {
+        return $this->redirect("https://docs.google.com/forms/d/e/1FAIpQLSdTDfoc_A0l7kuzL8wXl4fsxxDQtOgWLjFo1nNB2e4mdihsIQ/viewform?usp=sf_link");
     }
 }
