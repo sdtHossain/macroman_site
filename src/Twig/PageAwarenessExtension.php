126e4be45d3a6cef55ae038e69c3577536183162
Sarim Khan <sarim2005@gmail.com>
2021-02-15 03:46:25 +0600

    home related class/html logic implemented

---
+++ src/Twig/PageAwarenessExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Twig;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class PageAwarenessExtension extends AbstractExtension
+{
+    /** @var Request */
+    protected $req;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->req = $requestStack->getCurrentRequest();
+    }
+
+    public function getFilters(): array
+    {
+        return [
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('is_page', [$this, 'isPage']),
+        ];
+    }
+
+    public function isPage($name)
+    {
+        return $this->req->attributes->get('_route') == $name;
+    }
+}
