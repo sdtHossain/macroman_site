5c262cffb413b9c9aefdc9a657c5b34a1b65754b
Sarim Khan <sarim2005@gmail.com>
2021-02-14 08:03:01 +0600

    configure webpack for scss and img asset copy

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,20 @@ Encore
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
+    // Referencing Image files from a Template
+    // https://symfony.com/doc/current/frontend/encore/copy-files.html
+    .copyFiles({
+        from: './assets/macromanhq-html/assets/img',
+
+        // optional target path, relative to the output dir
+        to: 'img/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        // to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        pattern: /\.(png|jpg|jpeg)$/
+    })
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
@@ -53,7 +67,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -63,7 +77,7 @@ Encore
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+    .enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
