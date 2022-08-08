04a5a2abc14a8ae41908e178063da4a6b5429730
sdtHossain <mshossain509@gmail.com>
2022-08-08 11:31:21 +0600

    rename portfoliojs to res_accordion

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('portfoliojs', './assets/portfolio.js')
+    .addEntry('res_accordionjs', './assets/responsiveaccordion.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
